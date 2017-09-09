ca87d284d1e1925bfc680c09d320a5dd68a2587e
Lizzie Hurley <l.hurley58@gmail.com>
2017-09-08 22:25:04 -0500

    syntax errors solved

--- keys.js
+++ keys.js
@@ -6,12 +6,3 @@ exports.twitterKeys = {
   access_token_key: '906263603216601088-WmuiHb24XRRi44T0X3DMxqp30u0u6Rh',
   access_token_secret: 'PdW9xhH0ZqdCIOS36H418PthM8uTEWi0nteck2eufpYJL',
 }
-
-exports.spotifyKeys = {
-  clientId : '979eb5706da84ff89eab5e7d4bd68049',
-  clientSecret: '1be1cb69f2074e329ffde2115687d217',
-}
-
-exports.omdbKeys = {
-  consumer_key: '40e9cece',
-}
